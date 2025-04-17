ea524a0c7d6e66e3227c1b9b4620d82f02c2db3b
Federico Capoccia <166076838+FedericoCapoccia@users.noreply.github.com>
2025-04-17 13:19:58 +0200

    .

--- src/clibs.zig
+++ src/clibs.zig
@@ -1,3 +1,4 @@
 pub const c = @cImport({
+    @cInclude("vulkan/vulkan.h");
     @cInclude("vk_mem_alloc.h");
 });
--- src/main.zig
+++ src/main.zig
@@ -17,19 +17,6 @@ pub fn main() !void {
     try Engine.init(&engine);
     defer engine.shutdown();
 
-    // const window = Window.initialize(800, 600, "SimpleEngine") catch |err| {
-    //     log.err("Failed to create window: {s}", .{@errorName(err)});
-    //     return;
-    // };
-    // defer window.shutdown();
-    //
-    //
-    // var renderer = Renderer.initialize(allocator, &window) catch |err| {
-    //     log.err("Failed to create renderer: {s}", .{@errorName(err)});
-    //     return;
-    // };
-    // defer renderer.shutdown();
-    //
     // var running = true;
     // var event: sdl.Event = undefined;
     // while (running) {
