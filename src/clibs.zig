50439eee4d805bcc63ba50eb342722fe9b81fa56
Federico Capoccia <166076838+FedericoCapoccia@users.noreply.github.com>
2025-04-20 00:37:45 +0200

    added imgui to build system

--- src/clibs.zig
+++ src/clibs.zig
@@ -3,9 +3,9 @@ pub const c = @cImport({
     @cInclude("vk_mem_alloc.h");
     @cDefine("GLFW_INCLUDE_NONE", "");
     @cInclude("GLFW/glfw3.h");
-    @cInclude("cimgui.h");
-    @cInclude("cimgui_impl_vulkan.h");
-    @cInclude("cimgui_impl_glfw.h");
+    @cInclude("dcimgui.h");
+    @cInclude("dcimgui_impl_vulkan.h");
+    @cInclude("dcimgui_impl_glfw.h");
 });
 
 // pub extern fn glfwGetPhysicalDevicePresentationSupport(instance: vk.Instance, pdev: vk.PhysicalDevice, queuefamily: u32) c_int;
