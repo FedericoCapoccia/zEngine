701d47ce3946e6e385c850e5023778f2229462af
Federico Capoccia <166076838+FedericoCapoccia@users.noreply.github.com>
2025-04-25 23:08:02 +0200

    tidy up of the build system

--- build.zig
+++ build.zig
@@ -14,100 +14,127 @@ pub fn build(b: *std.Build) !void {
         .optimize = optimize,
         .link_libc = true,
     });
-    exe.linkLibCpp();
 
+    // ===================================================================
+    // [SECTION] GLFW Zig Bindings
+    // ===================================================================
     const zglfw = b.dependency("zglfw", .{
         .x11 = false,
         .wayland = true,
     });
     exe.root_module.addImport("zglfw", zglfw.module("root"));
+    exe.linkLibrary(zglfw.artifact("glfw"));
 
-    if (target.result.os.tag != .emscripten) {
-        exe.linkLibrary(zglfw.artifact("glfw"));
+    // ===================================================================
+    // [SECTION] Vulkan
+    // ===================================================================
+    {
+        switch (target.result.os.tag) {
+            .windows => {
+                const vulkan_sdk = env.get("VULKAN_SDK") orelse @panic("Failed to retrieve VULKAN_SDK env var");
+                exe.addLibraryPath(.{ .cwd_relative = std.fmt.allocPrint(b.allocator, "{s}/lib", .{vulkan_sdk}) catch unreachable });
+                exe.linkSystemLibrary("vulkan-1");
+                exe.linkSystemLibrary("gdi32");
+                exe.linkSystemLibrary("dwmapi");
+                exe.linkSystemLibrary("winmm");
+            },
+            .linux => exe.linkSystemLibrary("vulkan"),
+            else => @panic("Unsupported OS"),
+        }
+
+        const vulkan = b.dependency("vulkan_zig", .{
+            .registry = b.path("thirdparty/VulkanHeaders/registry/vk.xml"),
+        }).module("vulkan-zig");
+        exe.root_module.addImport("vulkan", vulkan);
     }
 
-    exe.addIncludePath(b.path("thirdparty/VulkanHeaders/include"));
-    exe.addIncludePath(b.path("thirdparty/VMA/include"));
-    //exe.addIncludePath(zglfw.artifact("glfw").getEmittedIncludeTree());
-    //exe.addIncludePath(b.path("thirdparty/imgui"));
-    exe.addCSourceFile(.{ .file = b.path("src/vulkan/vma.cpp"), .flags = &.{""} });
-
-    const imgui_lib = b.addStaticLibrary(.{
-        .name = "cimgui",
-        .target = target,
-        .optimize = optimize,
-    });
-    imgui_lib.addIncludePath(b.path("thirdparty/imgui"));
-    imgui_lib.addIncludePath(zglfw.artifact("glfw").getEmittedIncludeTree());
-    imgui_lib.linkLibCpp();
-    imgui_lib.addCSourceFiles(.{
-        .files = &.{
-            "thirdparty/imgui/imgui.cpp",
-            "thirdparty/imgui/imgui_demo.cpp",
-            "thirdparty/imgui/imgui_draw.cpp",
-            "thirdparty/imgui/imgui_tables.cpp",
-            "thirdparty/imgui/imgui_widgets.cpp",
-            "thirdparty/imgui/imgui_impl_glfw.cpp",
-            "thirdparty/imgui/imgui_impl_vulkan.cpp",
-            "thirdparty/imgui/dcimgui.cpp",
-            "thirdparty/imgui/dcimgui_impl_glfw.cpp",
-            "thirdparty/imgui/dcimgui_impl_vulkan.cpp",
-            "thirdparty/imgui/dcimgui_internal.cpp",
-        },
-        .flags = &.{"-DGLFW_INCLUDE_NONE"},
-    });
-    if (target.result.os.tag == .windows) {
-        const vulkan_sdk = env.get("VULKAN_SDK") orelse @panic("Failed to retrieve VULKAN_SDK env var");
-        imgui_lib.addLibraryPath(.{ .cwd_relative = std.fmt.allocPrint(b.allocator, "{s}/lib", .{vulkan_sdk}) catch unreachable });
-    }
-
-    imgui_lib.addIncludePath(b.path("thirdparty/VulkanHeaders/include"));
-    exe.linkLibrary(imgui_lib);
-
-    const c_translate = b.addTranslateC(.{
-        .root_source_file = b.addWriteFiles().add("c.h",
-            \\#define GLFW_INCLUDE_NONE
-            \\#include <GLFW/glfw3.h>
-            \\#include <vulkan/vulkan.h>
-            \\#include <vk_mem_alloc.h>
-            \\#include <dcimgui.h>
-            \\#include <dcimgui_impl_glfw.h>
-            \\#include <dcimgui_impl_vulkan.h>
-        ),
-        .target = target,
-        .optimize = optimize,
-        .link_libc = true,
-    });
-
-    c_translate.addIncludePath(zglfw.artifact("glfw").getEmittedIncludeTree());
-    c_translate.addIncludePath(b.path("thirdparty/VMA/include"));
-    c_translate.addIncludePath(b.path("thirdparty/VulkanHeaders/include"));
-    c_translate.addIncludePath(b.path("thirdparty/imgui"));
-    const c_mod = c_translate.createModule();
-    c_mod.addCSourceFile(.{ .file = b.path("src/vulkan/vma.cpp"), .flags = &.{""} });
-    exe.root_module.addImport("c", c_translate.createModule());
-
-    //=================================================================================================================
-    // Vulkan
-    //=================================================================================================================
-
-    switch (target.result.os.tag) {
-        .windows => {
+    // ===================================================================
+    // [SECTION] ImGui
+    // ===================================================================
+    {
+        const imgui = b.addLibrary(.{
+            .name = "imgui",
+            .linkage = .static,
+            .root_module = b.createModule(.{
+                .target = target,
+                .optimize = optimize,
+                .link_libc = true,
+                .link_libcpp = true,
+            }),
+        });
+        imgui.addIncludePath(b.path("thirdparty/imgui"));
+        imgui.addIncludePath(zglfw.artifact("glfw").getEmittedIncludeTree());
+        imgui.addIncludePath(b.path("thirdparty/VulkanHeaders/include"));
+        imgui.addCSourceFiles(.{
+            .files = &.{
+                "thirdparty/imgui/imgui.cpp",
+                "thirdparty/imgui/imgui_draw.cpp",
+                "thirdparty/imgui/imgui_widgets.cpp",
+                "thirdparty/imgui/imgui_tables.cpp",
+                "thirdparty/imgui/imgui_demo.cpp",
+                "thirdparty/imgui/imgui_impl_glfw.cpp",
+                "thirdparty/imgui/imgui_impl_vulkan.cpp",
+            },
+            .flags = &.{"-DGLFW_INCLUDE_NONE"},
+        });
+        if (target.result.os.tag == .windows) {
             const vulkan_sdk = env.get("VULKAN_SDK") orelse @panic("Failed to retrieve VULKAN_SDK env var");
-            exe.addLibraryPath(.{ .cwd_relative = std.fmt.allocPrint(b.allocator, "{s}/lib", .{vulkan_sdk}) catch unreachable });
-            exe.linkSystemLibrary("vulkan-1");
-            exe.linkSystemLibrary("gdi32");
-            exe.linkSystemLibrary("dwmapi");
-            exe.linkSystemLibrary("winmm");
-        },
-        .linux => exe.linkSystemLibrary("vulkan"),
-        else => @panic("Unsupported OS"),
+            imgui.addLibraryPath(.{ .cwd_relative = std.fmt.allocPrint(b.allocator, "{s}/lib", .{vulkan_sdk}) catch unreachable });
+        }
+        exe.linkLibrary(imgui);
     }
 
-    const vulkan = b.dependency("vulkan_zig", .{
-        .registry = b.path("thirdparty/VulkanHeaders/registry/vk.xml"),
-    }).module("vulkan-zig");
-    exe.root_module.addImport("vulkan", vulkan);
+    // ===================================================================
+    // [SECTION] VulkanMemoryAllocator
+    // ===================================================================
+    {
+        const vma = b.addLibrary(.{
+            .name = "VulkanMemoryAllocator",
+            .linkage = .static,
+            .root_module = b.createModule(.{
+                .target = target,
+                .optimize = optimize,
+                .link_libc = true,
+                .link_libcpp = true,
+            }),
+        });
+        vma.addIncludePath(b.path("thirdparty/VMA/include"));
+        vma.addIncludePath(b.path("thirdparty/VulkanHeaders/include"));
+        vma.addCSourceFile(.{
+            .file = b.addWriteFiles().add("vk_mem_alloc.cpp",
+                \\#include <vk_mem_alloc.h>
+            ),
+            .flags = &.{"-DVMA_IMPLEMENTATION"},
+        });
+        b.installArtifact(vma);
+        exe.linkLibrary(vma);
+    }
+
+    // ===================================================================
+    // [SECTION] C module
+    // ===================================================================
+    {
+        const c_translate = b.addTranslateC(.{
+            .root_source_file = b.addWriteFiles().add("c.h",
+                \\#define GLFW_INCLUDE_NONE
+                \\#include <GLFW/glfw3.h>
+                \\#include <vulkan/vulkan.h>
+                \\#include <vk_mem_alloc.h>
+                \\#include <dcimgui.h>
+                \\#include <dcimgui_impl_glfw.h>
+                \\#include <dcimgui_impl_vulkan.h>
+            ),
+            .target = target,
+            .optimize = optimize,
+            .link_libc = true,
+        });
+
+        c_translate.addIncludePath(zglfw.artifact("glfw").getEmittedIncludeTree());
+        c_translate.addIncludePath(b.path("thirdparty/VMA/include"));
+        c_translate.addIncludePath(b.path("thirdparty/VulkanHeaders/include"));
+        c_translate.addIncludePath(b.path("thirdparty/imgui"));
+        exe.root_module.addImport("c", c_translate.createModule());
+    }
 
     b.installArtifact(exe);
 
