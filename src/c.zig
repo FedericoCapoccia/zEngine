99f7feafffa8e1fe08f28b6b274ec1cca7eea06f
Federico Capoccia <166076838+FedericoCapoccia@users.noreply.github.com>
2025-04-26 02:07:52 +0200

    old files cleanup

--- src/c.zig
+++
@@ -1,9 +0,0 @@
-pub const headers = @cImport({
-    @cDefine("GLFW_INCLUDE_NONE", "");
-    @cInclude("GLFW/glfw3.h");
-    @cInclude("vulkan/vulkan.h");
-    @cInclude("vk_mem_alloc.h");
-    @cInclude("dcimgui.h");
-    @cInclude("dcimgui_impl_vulkan.h");
-    @cInclude("dcimgui_impl_glfw.h");
-});
